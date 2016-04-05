7a1e231ed91df7dd386c911efa0442746c9797fe
Cole Mickens <cole.mickens@gmail.com>
2016-04-04 22:02:08 -0700

    nixcfg: enable dri3

--- nixcfg/profiles/pc/default.nix
+++ nixcfg/profiles/pc/default.nix
@@ -31,6 +31,9 @@
       layout = "us";
       libinput.enable = true;
       useGlamor = true;
+      deviceSection = ''
+        Option "DRI" "3"
+      '';
     };
     udev.extraRules = ''
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess"
