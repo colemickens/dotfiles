f47f39b20d54144a20785cc65d7ce2c0f9bb77e9
Cole Mickens <cole.mickens@gmail.com>
2016-11-03 20:37:12 -0700

    add az sub helpers

--- homedir/.zsh/lib/azure.zsh
+++ homedir/.zsh/lib/azure.zsh
@@ -1,1 +1,7 @@
-#source <(azure --completion)
+source <(azure --completion)
+
+alias az_acsdevtest="az account set --name=c1089427-83d3-4286-9f35-5af546a6eb67"
+alias az_anthonysub="az account set --name=b52fce95-de5f-4b37-afca-db203a5d0b6a"
+alias az_linuxdevtest="az account set --name=27b750cd-ed43-42fd-9044-8d75e124ae55"
+alias az_kubetesting="az acccount set --name=6f368760-9ad2-4aef-8ff1-fb038d2e75bf"
+alias az_redhat="az account set --name=1075fe32-a1d3-4f14-b907-31c0539a91d1"
