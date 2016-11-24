e5a1af1747c91b78a9374b9d43d5ceb274a1366e
Cole Mickens <cole.mickens@gmail.com>
2016-11-24 00:04:28 -0600

    pixel rearrange

--- homedir/.zsh/lib/pixel.zsh
+++ homedir/.zsh/lib/pixel.zsh
@@ -1,26 +1,13 @@
 pixel_speakers() {
-	amixer -c 0 cset name='DAC1 MIXL DAC1 Switch' on
-	amixer -c 0 cset name='DAC1 MIXR DAC1 Switch' on
-
-	amixer -c 0 cset name='Headphone Switch' off
-	amixer -c 0 cset name='Stereo DAC MIXL DAC1 L Switch' off
-	amixer -c 0 cset name='Stereo DAC MIXR DAC1 R Switch' off
-	amixer -c 0 cset name='Stereo DAC MIXL DAC1 R Switch' on
-	amixer -c 0 cset name='Stereo DAC MIXR DAC1 L Switch' on
-	amixer -c 0 cset name='Speaker Switch' on
+	$HOME/code/raphael/linux-samus/scripts/setup/audio/enable-audio.sh
+	$HOME/code/raphael/linux-samus/scripts/setup/audio/enable-speakers.sh
 }
 
 pixel_headphones() {
-	amixer -c 0 cset name='DAC1 MIXL DAC1 Switch' on
-	amixer -c 0 cset name='DAC1 MIXR DAC1 Switch' on
-
-	amixer -c 0 cset name='Speaker Switch' off
-	amixer -c 0 cset name='Stereo DAC MIXL DAC1 R Switch' off
-	amixer -c 0 cset name='Stereo DAC MIXR DAC1 L Switch' off
-	amixer -c 0 cset name='Stereo DAC MIXL DAC1 L Switch' on
-	amixer -c 0 cset name='Stereo DAC MIXR DAC1 R Switch' on
-	amixer -c 0 cset name='OUT1 Playback Switch' on
-	amixer -c 0 cset name='OUT2 Playback Switch' on
-	amixer -c 0 cset name='Headphone Switch' on
+	$HOME/code/raphael/linux-samus/scripts/setup/audio/enable-audio.sh
+	$HOME/code/raphael/linux-samus/scripts/setup/audio/enable-headphones.sh
 }
 
+pixel_touchpad() {
+	$HOME/code/raphael/linux-samus/scripts/setup/touchpad/enable-atmel.sh
+}
