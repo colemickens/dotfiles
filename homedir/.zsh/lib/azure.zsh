7403fb0cec8cd42386eefa19b0085fb490ad56d6
Cole Mickens <cole.mickens@gmail.com>
2017-06-20 21:23:05 +0000

    make work in bash and w/o parallel

--- homedir/.zsh/lib/azure.zsh
+++ homedir/.zsh/lib/azure.zsh
@@ -1,32 +1,40 @@
 source <(azure --completion)
 source az.completion.sh &>/dev/null
 
-az_anthony() { az account set --subscription=$(az account list --all --query "[?contains(name, 'Anthony')] | [0].id" --output tsv); az account show }
-az_acsdev() { az account set --subscription=$(az account list --all --query "[?contains(name, 'Container Service - Development')] | [0].id" --output tsv); az account show }
-az_acstest() { az account set --subscription=$(az account list --all --query "[?contains(name, 'Container Service - Test')] | [0].id" --output tsv); az account show }
+az_anthony () { az account set --subscription=$(az account list --all --query "[?contains(name, 'Anthony')] | [0].id" --output tsv); az account show; }
+az_acsdev () { az account set --subscription=$(az account list --all --query "[?contains(name, 'Container Service - Development')] | [0].id" --output tsv); az account show; }
+az_acstest () { az account set --subscription=$(az account list --all --query "[?contains(name, 'Container Service - Test')] | [0].id" --output tsv); az account show; } 
 
-az_cleanup() {(
+az_cleanup () {(
 	pattern="${1:-"colemick"}"
 	list="$(\
 		az group list \
 			--output tsv \
 			--query "[?tags.colemickpermanent != 'true' && contains(name, '${pattern}') && properties.provisioningState != 'Deleting'].name")"
 
-	printf "${list}" | parallel 'printf "Will delete: %s\n" {}'
+	if which parallel; then
+		printf "${list}" | parallel 'printf "Will delete: %s\n" {}'
+	else
+		echo "Will delete ${list}"
+	fi
 	printf "Are you sure? [y/N] "
 	read -r response
 	if [[ "${response:l}" =~ ^(yes|y)$ ]]; then
-		printf "${list}" | parallel 'printf "Deleting %s\n" {} && az group delete --no-wait --yes --name {}'
+		if which parallel ; then
+			printf "${list}" | parallel 'printf "Deleting %s\n" {} && az group delete --no-wait --yes --name {}'
+		else
+			echo "${list}"| while IFS= read -r line ; do az group delete --no-wait --yes --name "${line}"; done
+		fi
 	fi
 )}
 
-_dumplocation() {
+_dumplocation () {
   location=$1
   az vm list-usage --location=$location \
     | jq -r ".[] | [\"$location\", .name.localizedValue, \" \", .limit, .currentValue] | @tsv"
 }
 
-az_dumplocation() {
+az_dumplocation () {
 	if [[ -z "${1}" ]]; then
 		az account list-locations --query "[].name" | jq -r '.[]' \
 			| while read x; do _dumplocation $x; done
