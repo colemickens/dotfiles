7e010309549fc246b3327d9ad2b0dca3a1baf556
Cole Mickens <cole.mickens@gmail.com>
2017-10-06 21:22:24 -0700

    ssh.zsh: cleanup old aliases

--- homedir/.zsh/lib/ssh.zsh
+++ homedir/.zsh/lib/ssh.zsh
@@ -2,18 +2,14 @@ assh() {
 	autossh -M 0 "$@" -o "ServerAliveInterval 45" -o "ServerAliveCountMax 2"
 }
 
-autossh_chimera_remote()	{ assh	cole@mickens.io			-p 222 }
-autossh_chimera_local()		{ assh	cole@chimera.local		-p 222 }
-autossh_azdev()				{ assh	cole@azdev.mickens.io	-p 22 }
-ssh_chimera_remote()		{ ssh	cole@mickens.io			-p 222 }
-ssh_chimera_local()			{ ssh	cole@chimera.local		-p 222 }
-ssh_azdev()					{ ssh	cole@azdev.mickens.io	-p 22 }
-mosh_chimera_remote()		{ mosh	cole@chimera.mickens.io	--ssh="ssh -p 222" }
-mosh_chimera_local()		{ mosh	cole@chimera.local		--ssh="ssh -p 222" }
-mosh_azdev()				{ mosh	cole@azdev.mickens.io	}
+autossh_chimera()	{ assh	cole@chimera.mickens.io -p 222 }
+ssh_chimera()		{ ssh	cole@chimera.mickens.io -p 222 }
+mosh_chimera()		{ mosh	cole@chimera.mickens.io	--ssh="ssh -p 222" }
+
+socks_chimera() { set -x; ssh -v -D 1080 -C -q -N cole@chimera.mickens.io -p 222 }
 
 proxy_chimera_rev() { assh cole@chimera.mickens.io -p 222 -N -T -R 2222:localhost:${1} }
 proxy_chimera_fwd() { assh cole@chimera.mickens.io -p 222 -N -T -L 2222:localhost:2222 }
 proxy_connect() { assh cole@localhost -p 2222 }
 
-sshuttle_chimera() { sshuttle -r cole@mickens.io:222 '0.0.0.0/0' }
+sshuttle_chimera() { sshuttle -r cole@chimera.mickens.io:222 '0.0.0.0/0' }
