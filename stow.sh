463d20a984f08dbaa898d3f0e4207e8e9a484a08
Cole Mickens <cole.mickens@gmail.com>
2019-01-10 23:46:58 -0800

    update stow to smartly unlock

--- stow.sh
+++ stow.sh
@@ -5,7 +5,10 @@ set -e # we must bail if we fail to `git crypt unlock`, etc
 
 stow --no-folding -t $HOME/ homedir
 
-git crypt unlock
+if git ls-tree -r HEAD --name-only | sed 's/.*/"&"/' | xargs grep -qsPa "\x00GITCRYPT" ; then
+  git crypt unlock
+fi
+
 stow --no-folding -t $HOME/ homedir-enc
 
 # run init stuff here, detect if we should skip
